b0dbd92a51dd4cf2b79271fe17232774f6c221c7
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-02 22:59:01 +0300

    javascript

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function calculateLoan() {
-    loanAmountValue = document.getElementById("loan-amount").value
+    loanAmountValue = document.getElementById("loan-amount").value;
 
-    interestRateValue = document.getElementById("interest-rate").value
+    interestRateValue = document.getElementById("interest-rate").value;
 
-    MonthsToPayValue = document.getElementById("months-to-pay").value
+    MonthsToPayValue = document.getElementById("months-to-pay").value;
 
-    interest =(loanAmountValue * (interestRateValue * 0.01)) /MonthsToPayValue
+    interest =(loanAmountValue * (interestRateValue * 0.01)) /MonthsToPayValue;
 
-    monthlyPayment = (loanAmountValue / MonthsToPayValue + interest)
+    monthlyPayment = (loanAmountValue / MonthsToPayValue + interest).toFixed(2);
+
+    document.getElementById("payment").innerHTML = `Monthly Payment: ${monthlyPayment}`
 }
